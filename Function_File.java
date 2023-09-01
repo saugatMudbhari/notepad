ec95404100fe84e95318ac9605c2cb4b294e9922
saugat <smudbhari85@gmail.com>
2023-09-01 10:24:02 +0545

    load and read a file

--- Function_File.java
+++ Function_File.java
@@ -1,7 +1,11 @@
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Function_File {
     frame frame;
+    String fileName;
+    String fileAddress;
 
     public Function_File(frame frame) {
         this.frame = frame;
@@ -11,9 +15,25 @@ public class Function_File {
         frame.textArea.setText("");
         frame.f.setTitle("New");
     }
-    public void open()
-    {
-        FileDialog fd= new FileDialog(frame.f,"Open",FileDialog.LOAD);
+
+    public void open() {
+        FileDialog fd = new FileDialog(frame.f, "Open", FileDialog.LOAD);
         fd.setVisible(true);
+        if (fd.getFile() != null) {
+            fileName = fd.getFile();
+            fileAddress = fd.getDirectory();
+            frame.f.setTitle(fileName);
+        }
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileAddress + fileName));
+            frame.textArea.setText("");
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                frame.textArea.append(line + '\n');
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("FILE NOT OPENED");
+        }
     }
 }
